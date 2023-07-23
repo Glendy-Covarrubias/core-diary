ca0b8a11f0e68b3665bccb1616ee3f07d515f566
Glendy-Covarrubias <glendycovarrubias@hotmail.com>
2023-07-22 20:23:34 -0600

    SinReq Acompletando Crud
    -Metodo update
    -Metodo delete

--- src/controllers/diaryControllers.ts
+++ src/controllers/diaryControllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { getDiariesServices, setDiariesServices } from "../services/diaryServices";
+import { getDiariesServices, setDiariesServices, deleteDiariesServices, updateDiariesServices, searchDiariesServices } from "../services/diaryServices";
 
 export async function getDiariesController(req: Request, res: Response) {
     try {
@@ -15,8 +15,41 @@ export async function getDiariesController(req: Request, res: Response) {
 export async function setDiariesController(req: Request, res: Response) {
     try {
         const newDiary = await setDiariesServices(req.body);
-        console.log("ConsoleLogCreate: ", req, newDiary, res);
         res.status(200).json({ status: 200, message: "success", data: newDiary });
+    } catch (error: any) {
+        res.status(500).json({ status: 500, message: error.message });
+    }
+}
+
+export async function updateDiariesController(req: Request, res: Response) {
+    try {
+        const idDiary = req.params.id;
+        const updateDiary = req.body;
+        await updateDiariesServices(idDiary, updateDiary);
+        console.log(idDiary, updateDiary);
+        res.status(200).json({ status: 200, message: "success", data: updateDiary });
+    } catch (error: any) {
+        res.status(500).json({ status: 500, message: error.message });
+    }
+}
+
+export async function deleteDiariesController(req: Request, res: Response) {
+    try {
+        const idDiary = req.params.id;
+        await deleteDiariesServices(idDiary);
+        res.status(200).json({ status: 200, message: "success", data: { id: idDiary } });
+    } catch (error: any) {
+        res.status(500).json({ status: 500, message: error.message });
+    }
+}
+
+export async function searchDiaryController(req: Request, res: Response) {
+    try {
+        const idDiary = parseInt(req.params.id);
+        console.log(idDiary); /**Pendiente revisar */
+        const result = await searchDiariesServices(1);
+        console.log(result);
+        res.status(200).json({ status: 200, message: "success", data: result });
     } catch (error: any) {
         res.status(500).json({ status: 500, message: error.message });
     }
--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -2,15 +2,47 @@ const Diary = require('../models/Diary');
 
 export async function getDiariesServices() {
   try {
-    return await Diary.findAll();
+    const options = {
+      order: [['id', 'ASC']]
+    };
+    return await Diary.findAll(options);
   } catch (error: any) {
     throw Error(error);
   }
 }
 
-export async function setDiariesServices(data : any) {
+export async function setDiariesServices(data: any) {
   try {
     return await Diary.create(data);
+  } catch (error: any) {
+    throw Error(error);
+  }
+}
+
+export async function updateDiariesServices(idDiary: any, data: any) {
+  try {
+    const diary = await Diary.findByPk(idDiary);
+    diary.name = data.name;
+    diary.priority = data.priority;
+    diary.status = data.status;
+    diary.description = data.description;
+    return await diary.save();
+  } catch (error: any) {
+    throw Error(error);
+  }
+}
+
+export async function deleteDiariesServices(idDiary: any) {
+  try {
+    return await Diary.destroy({ where: { id: idDiary } });
+  } catch (error: any) {
+    throw Error(error);
+  }
+}
+
+export async function searchDiariesServices(idDiary: any) {
+  try {
+    return await Diary.findByPk(idDiary);
   } catch (error: any) {
     throw Error(error);
   }
